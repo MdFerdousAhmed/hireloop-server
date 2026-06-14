b8c9e4ee7d9e302cd304dd21876a921304d6271d
MD FERDOUS AHMED <148644652+MdFerdousAhmed@users.noreply.github.com>
2026-06-14 14:23:42 +0600

    fixed some problem

--- index.js
+++ index.js
@@ -349,7 +349,7 @@ async function run() {
     })
 
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     app.listen(port, () => {
